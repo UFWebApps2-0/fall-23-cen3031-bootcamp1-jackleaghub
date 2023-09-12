164dc432e87ace452a70e144ed7564c2f08b6261
jacklea <jacklea@ufl.edu>
2023-09-11 21:27:09 -0400

    done

--- server.js
+++ server.js
@@ -9,8 +9,17 @@ var requestHandler = function(request, response) {
   /*Investigate the request object. 
     You will need to use several of its properties: url and method
   */
-  //console.log(request);
-
+	//console.log(request.url);
+	
+	if (request.method == 'GET' && request.url == '/listings') {
+		response.writeHead(200);
+		response.end(JSON.stringify(listingData));
+	}
+	else {
+		response.writeHead(404);
+		response.end('Bad gateway error - path');
+	}
+	
   /*
     Your request handler should send listingData in the JSON format as a response if a GET request 
     is sent to the '/listings' path. Otherwise, it should send a 404 error. 
@@ -42,19 +51,23 @@ fs.readFile('listings.json', 'utf8', function(err, data) {
     HINT: Read up on JSON parsing Node.js
     http://stackoverflow.com/questions/17251553/nodejs-request-object-documentation
    */
-
-    //Check for errors
-    /*this resource gives you an idea of the general format err objects and Throwing an existing object.
+	
+    // Check for errors
+	if (err) throw err;
+	
+    /* this resource gives you an idea of the general format err objects and Throwing an existing object.
     https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/throw#throwing_an_existing_object
    */
   
-
    //Save the data in the listingData variable already defined
-  
+  	listingData = JSON.parse(data);
 
   //Creates the server
-  
+	server = http.createServer(requestHandler);
+	
   //Start the server
-
+	server.listen(port, function() {
+		console.log('Server listening on: http://127.0.0.1:' + port);	
+	});
 
 });
